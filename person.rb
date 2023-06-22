f91ed9d3ec1ca0d00b4529c35c11b3a7eb7ac510
adarachel <adarachelofficial@gmail.com>
2023-06-22 18:38:05 +0100

    Update person.rb

--- person.rb
+++ person.rb
@@ -4,6 +4,7 @@ class Person < Nameable
   attr_accessor :name
 
   def initialize(name = 'Unknown', age = nil, parent_permission: true)
+    super()
     @name = name
     @age = age
     @parent_permission = parent_permission
