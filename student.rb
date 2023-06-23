2c6b786f39e0033df9b6cc33de215e675b58bf73
adarachel <adarachelofficial@gmail.com>
2023-06-23 16:17:49 +0100

    Project 3: set up associations.

--- student.rb
+++ student.rb
@@ -1,9 +1,13 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, name = 'Unknown', age = nil, parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
+    classroom.students << self
   end
 
   def play_hooky
